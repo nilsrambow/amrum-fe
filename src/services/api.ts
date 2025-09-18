74d86994067c4b89a901a71d1548749381a54506
nilsrambow <nilsbrodersen@MON-PF4TNVZB-DE.localdomain>
2025-09-18 15:55:53 +0200

    ...

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // API Configuration
-const API_BASE_URL = "http://0.0.0.0:8888";
+const API_BASE_URL = "http://localhost:8123";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
