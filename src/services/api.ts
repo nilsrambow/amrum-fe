c0ef8081b7316acef5680cfb20d1da0064936c0a
nilsbroders <nils_brodersen@posteo.de>
2025-09-18 23:07:10 +0200

    still fixing hosts between FE and BE

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // API Configuration
-const API_BASE_URL = "http://localhost:8223";
+const API_BASE_URL = "http://amrum-be:8000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
